8f3b6596b98b8bb93d05d3913a6704071ca9a5b7
visualdiary <68695465+visualdiary@users.noreply.github.com>
2020-07-27 11:01:57 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello
+{
+  public static void main()
+  {
+      Scanner sc=new Scanner(System.in);
+      int a,s=0;
+      System.out.println("enter a number");
+      a=sc.nextInt();
+      s=s+a;
+      System.out.println("Sum="+s);
+      }
+      }
