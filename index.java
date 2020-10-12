bc7a5ea30ced11a9cc0aa1a5e2a984ec340b40f1
visualdiary <68695465+visualdiary@users.noreply.github.com>
2020-10-12 10:33:58 +0530

    Update index.java

--- index.java
+++ index.java
@@ -1,13 +1,31 @@
-import java.util.*;
-public class Hello
-{
-  public static void main()
-  {
-      Scanner sc=new Scanner(System.in);
-      int a,s=0;
-      System.out.println("enter a number");
-      a=sc.nextInt();
-      s=s+a;
-      System.out.println("Sum="+s);
-      }
-      }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class Employee{
+   String name;
+   int id;
+   int age;
+   Employee(String name, int age, int id){
+      this.name = name;
+      this.age = age;
+      this.id = id;
+   }
+   public void displayDetails(){
+      System.out.println("Name: "+this.name);
+      System.out.println("Age: "+this.age);
+      System.out.println("Id: "+this.id);
+   }
+}
+public class ReadData {
+   public static void main(String args[]) throws IOException {
+      BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+      System.out.println("Enter your name: ");
+      String name = reader.readLine();
+      System.out.println("Enter your age: ");
+      int age = Integer.parseInt(reader.readLine());
+      System.out.println("Enter your Id: ");
+      int id = Integer.parseInt(reader.readLine());
+      Employee std = new Employee(name, age, id);
+      std.displayDetails();
+   }
+}
